a7e33b0d50530cf9196a380ae51d8f6824c81503
cvdtc <info.cvdtc@gmail.com>
2019-05-11 09:32:03 +0700

    Update 11052019
    - update insert tujuan + idkodepos dari database sigk
    - list kodepos dari database sigk
    - list tujuan menampilkan kelurahan, kecamata, kota, dan provinsi dari databse sigk

--- i_tujuan.php
+++ i_tujuan.php
@@ -7,11 +7,13 @@
         $kodepos = $_POST['kodepos'];
         $kota = $_POST['kota'];
         $idcustomer = $_POST['idcustomer'];
+        $idkodepos = $_POST['idkodepos'];
         
-        $sql = "INSERT INTO tujuan (alamat, kodepos, kota, CUS_ID) values ('$alamat', '$kodepos', '$kota', '$idcustomer');";
+        $sql = "INSERT INTO tujuan (alamat, kodepos, kota, CUS_ID, idkodepos) values ('$alamat', '$kodepos', '$kota', '$idcustomer', '$idkodepos');";
     
         if(mysqli_multi_query($con,$sql)){
             echo 'Anda Berhasil Menambahkan Tujuan';
+            echo $sql;
         }else{
         echo $sql;
             echo 'Tujuan Anda Gagal Ditambahkan, Harap ulangi...';
--- l_kodepos.php
+++ l_kodepos.php
@@ -2,24 +2,18 @@
   
     require_once('koneksi.php');
     
-    $id = $_GET['keysales'];
-    
-    $sql = "SELECT * FROM tujuan t, kodepos k WHERE t.idkodepos=k.KODEPOS and t.CUS_ID='$id';";
+    $sql = "SELECT * FROM kodepos;";
     $r = mysqli_query($con, $sql);
         
     $result = array();
         
     while($row = mysqli_fetch_array($r)){
         array_push($result,array(
-            "idtujuan"=>$row['idtujuan'],
-            "alamat"=>$row['alamat'],
-            "kodepos"=>$row['kodepos'],
-            "idcustomer"=>$row['CUS_ID'],
-            "idkodepos"=>$row['idkodepos'],
+            "kodepos"=>$row['KODEPOS'],
             "kelurahan"=>$row['KELURAHAN'],
             "kecamatan"=>$row['KECAMATAN'],
             "kota"=>$row['KOTA'],
-            "provinsi"=>$row['PROPINSI']
+            "provinsi"=>$row['PROPINSI'],
         ));
     }
 
